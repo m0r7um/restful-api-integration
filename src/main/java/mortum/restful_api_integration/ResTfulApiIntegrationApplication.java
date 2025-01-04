ab1374a66e4bba641e880557168249ce3a1f24e5
Мартин Вальц <martin@MacBook-Pro-Martin.local>
2025-01-04 21:19:17 +0300

    Initial commit

---
+++ src/main/java/mortum/restful_api_integration/ResTfulApiIntegrationApplication.java
@@ -0,0 +1,13 @@
+package mortum.restful_api_integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResTfulApiIntegrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResTfulApiIntegrationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mortum/restful_api_integration/ResTfulApiIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package mortum.restful_api_integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResTfulApiIntegrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
